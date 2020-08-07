6ca2d1ef0d9f67c9df38c567fb4d065446fa094d
HeZe <heze_heze@icloud.com>
2020-08-07 14:18:23 +0800

    Add files via upload

---
+++ 实船/catkin_ws/build/pubvel/CMakeFiles/test_vtg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_vtg.dir/pubvel.cpp.o"
+  "/home/deepdriving/catkin_ws/devel/lib/pubvel/test_vtg.pdb"
+  "/home/deepdriving/catkin_ws/devel/lib/pubvel/test_vtg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_vtg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 实船/catkin_ws/build/pubvel/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/deepdriving/catkin_ws/src/pubvel
+# Build directory: /home/deepdriving/catkin_ws/build/pubvel
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
