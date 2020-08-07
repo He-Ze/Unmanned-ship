003a91dd5e05c651210eb8d49561161fdd535c78
HeZe <heze_heze@icloud.com>
2020-08-07 10:00:06 +0800

    Add files via upload

---
+++ 实船/python_ws/build/boat/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/deepdriving/python_ws/src/boat
+# Build directory: /home/deepdriving/python_ws/build/boat
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
