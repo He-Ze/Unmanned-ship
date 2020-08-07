356556b6252237405837fef1ef63b17882b9f1db
HeZe <heze_heze@icloud.com>
2020-08-07 11:28:32 +0800

    Add files via upload

---
+++ 实船/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,23 @@
+# CMake generated Testfile for 
+# Source directory: /home/deepdriving/catkin_ws/src
+# Build directory: /home/deepdriving/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(boat_control/src/gpfpd_msgs)
+subdirs(navsatsignalparser/src/unionstrong_driver/unionstrong_driver)
+subdirs(navsatsignalparser/src/unionstrong_driver/unionstrong_msgs)
+subdirs(mapviz_and_control_station/controler_display)
+subdirs(HesaiLidar-ros)
+subdirs(boat_control/src/imu)
+subdirs(send_gps)
+subdirs(sim_boat)
+subdirs(boat_control/src/boat)
+subdirs(mapviz_and_control_station/industrial_pc)
+subdirs(mapviz_and_control_station/mapviz)
+subdirs(mapviz_and_control_station/multires_image)
+subdirs(mapviz_and_control_station/mapviz_plugins)
+subdirs(mapviz_and_control_station/tile_map)
+subdirs(track)
+subdirs(mapviz_and_control_station/usb_cam-develop)
---
+++ 实船/catkin_ws/build/w_vtg/CMakeFiles/wvtg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wvtg.dir/wvtg.cpp.o"
+  "/home/deepdriving/catkin_ws/devel/lib/w_vtg/wvtg.pdb"
+  "/home/deepdriving/catkin_ws/devel/lib/w_vtg/wvtg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wvtg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
