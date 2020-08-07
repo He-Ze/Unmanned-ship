c352f702987f2f5bc49593ec4a8c44f3d35a24b5
HeZe <heze_heze@icloud.com>
2020-08-07 11:32:49 +0800

    Add files via upload

---
+++ 实船/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/deepdriving/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/deepdriving/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/deepdriving/catkin_ws/devel;/opt/ros/kinetic" != "" else []
